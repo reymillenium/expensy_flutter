28aad1c96f853e036a5c4de26e789cb6e75f1e3c
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-04-30 17:27:30 -0400

    Installed the audioplayers package. Added the SoundHelper

---
+++ lib/helpers/sound_helper.dart
@@ -0,0 +1,21 @@
+// Packages:
+// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+// Helpers:
+
+// Utilities:
+
+class SoundEffectsHelper {
+  final AudioCache player = AudioCache(prefix: 'assets/audio/');
+
+  // Plays a click sound:
+  void playSmallButtonClick({double volume = 1.00}) {
+    playNote(fileName: 'zapsplat_multimedia_notification_pop_message_tooltip_small_click_007_63077.mp3', volume: volume);
+  }
+
+  // Plays a sound from a given file name and the volume:
+  void playNote({String fileName, double volume = 1.00}) {
+    player.play(fileName, volume: volume);
+  }
+}
