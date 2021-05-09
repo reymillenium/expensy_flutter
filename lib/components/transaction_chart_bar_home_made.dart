f89834d64a95376c9a22d32d792944ce7345a05d
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-05-08 20:04:34 -0400

    Modified the lib/components/transaction_chart_bar_home_made.dart by adding resizing of the elements of the UI based on the received constraints, besides the position of the device

--- lib/components/transaction_chart_bar_home_made.dart
+++ lib/components/transaction_chart_bar_home_made.dart
@@ -57,73 +57,84 @@ class TransactionChartBarHomeMade extends StatelessWidget {
     // bool isLandscape = DeviceHelper.isLandscape(orientation);
     bool isPortrait = DeviceHelper.isPortrait(orientation);
 
-    return Column(
-      children: [
-        // SizedBox(
-        //   height: 10,
-        // ),
-        if (isPortrait) ...[
-          Container(
-            child: FittedBox(
-              fit: BoxFit.fitWidth,
-              child: Text('${NumericHelper.roundDouble(groupedAmountOnDay['amount'], 2)}'),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-        ],
-        Expanded(
-          child: Stack(
-            alignment: AlignmentDirectional.bottomStart,
-            children: <Widget>[
-              Tooltip(
-                // message: '${NumericHelper.roundDouble(groupedAmountOnDay['amount'], 2)}',
-                message: amountLabel,
-
-                child: Container(
-                  // height: backgroundColumnHeight,
-                  width: 20,
-                  decoration: BoxDecoration(
-                    color: TinyColor(primaryColor).lighten(16).color,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    // borderRadius: BorderRadius.circular(10),
-                  ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          children: [
+            // SizedBox(
+            //   height: 10,
+            // ),
+            if (isPortrait) ...[
+              Container(
+                height: constraints.maxHeight * 0.10,
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text('${NumericHelper.roundDouble(groupedAmountOnDay['amount'], 2)}'),
                 ),
               ),
-              Tooltip(
-                message: amountLabel,
-                child: Container(
-                  height: activeBarHeight,
-                  width: 20,
-                  decoration: BoxDecoration(
-                    // color: Theme.of(context).accentColor,
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                ),
+              SizedBox(
+                height: constraints.maxHeight * 0.05,
               ),
             ],
-          ),
-        ),
-        if (isPortrait) ...[
-          SizedBox(
-            height: 10,
-          ),
-        ],
 
-        FittedBox(
-          child: Text(
-            groupedAmountOnDay['day'].substring(0, 2),
-            style: Theme.of(context).textTheme.headline6.copyWith(
-                  color: Theme.of(context).primaryColor,
+            // Bars
+            Container(
+              height: constraints.maxHeight * (isPortrait ? 0.67 : 0.84),
+              child: Stack(
+                alignment: AlignmentDirectional.bottomStart,
+                children: <Widget>[
+                  Tooltip(
+                    // message: '${NumericHelper.roundDouble(groupedAmountOnDay['amount'], 2)}',
+                    message: amountLabel,
+
+                    child: Container(
+                      // height: backgroundColumnHeight,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        color: TinyColor(primaryColor).lighten(16).color,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        // borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  Tooltip(
+                    message: amountLabel,
+                    child: Container(
+                      height: activeBarHeight,
+                      width: 20,
+                      decoration: BoxDecoration(
+                        // color: Theme.of(context).accentColor,
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (isPortrait) ...[
+              SizedBox(
+                height: constraints.maxHeight * 0.05,
+              ),
+            ],
+
+            Container(
+              height: constraints.maxHeight * (isPortrait ? 0.10 : 0.16),
+              child: FittedBox(
+                child: Text(
+                  groupedAmountOnDay['day'].substring(0, 2),
+                  style: Theme.of(context).textTheme.headline6.copyWith(
+                        color: Theme.of(context).primaryColor,
+                      ),
                 ),
-          ),
-        ),
-        // SizedBox(
-        //   height: 10,
-        // ),
-      ],
+              ),
+            ),
+            // SizedBox(
+            //   height: 10,
+            // ),
+          ],
+        );
+      },
     );
   }
 }
